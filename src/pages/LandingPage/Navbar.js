5a45e63cc01de32815566cdde682cf13b6ab978b
RuchiRay <ruchiray18@gmail.com>
2023-10-19 20:37:43 +0530

    removed how it works

--- src/pages/LandingPage/Navbar.js
+++ src/pages/LandingPage/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {  Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import logo from "../../img/logo.png";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { FaUser } from 'react-icons/fa'
 import { logout, reset } from '../../features/auth/authSlice'
 import { Link, useNavigate } from 'react-router-dom'
@@ -17,7 +17,7 @@ export const Navbar = () => {
     dispatch(reset())
     navigate('/')
   }
-console.log(user);
+  console.log(user);
   return (
     <div>
       <div className="flex justify-between w-full items-center text-orange-400 font-medium py-6 px-12 bg-orange-100">
@@ -33,21 +33,21 @@ console.log(user);
             <a href="#services" className="hover:underline">
               Services
             </a>
-            <a href="#how" className="hover:underline">
+            {/* <a href="#how" className="hover:underline">
               How it works
-            </a>
+            </a> */}
           </div>
         ) : (
           ""
         )}
-       {
-        user && <div className="flex gap-4 items-center">
-          <div><FaUser/></div>
-          <p className="font-semibold text-lg">{user.name}</p>
-          <Link to={'/dashboard'} className="bg-orange-400 text-orange-100 px-4 py-2 rounded-md">Dashboard</Link>
-          <button onClick={onLogout} className="border-orange-400 border-2  text-orange-400 px-4 py-2 rounded-md">Logout</button>
-        </div>
-       }
+        {
+          user && <div className="flex gap-4 items-center">
+            <div><FaUser /></div>
+            <p className="font-semibold text-lg">{user.name}</p>
+            <Link to={'/dashboard'} className="bg-orange-400 text-orange-100 px-4 py-2 rounded-md">Dashboard</Link>
+            <button onClick={onLogout} className="border-orange-400 border-2  text-orange-400 px-4 py-2 rounded-md">Logout</button>
+          </div>
+        }
       </div>
       <Outlet />
     </div>
